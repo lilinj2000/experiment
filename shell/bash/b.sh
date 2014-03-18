26b331b69ccc3284b2c36a04737ce5a3cbecceb7
lilinj2000 <lilinj2000@163.com>
2014-03-18 16:36:46 +0800

    this bash shell.

---
+++ shell/bash/b.sh
@@ -0,0 +1,2 @@
+#cp fdspm.log2008-10-23.log t.log
+cp `date '+fdspm.log%Y-%m-%d.log'` t.log
---
+++ shell/bash/c.sh
@@ -0,0 +1,4 @@
+#echo "" > fdspm.log2008-10-23.log
+#echo "" > eventlog
+#echo "" > omlog
+echo "" > `date '+fdspm.log%Y-%m-%d.log'`
